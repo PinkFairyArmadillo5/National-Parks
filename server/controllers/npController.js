7bfdb2f9c00f9002e51b27cb9a02058393e6b34e
Mia Kang <miakangnyc@gmail.com>
2022-01-02 13:25:01 -0500

    added controllers for put request to sql bucklist table, error on retrieving from sql bucketlist table

--- server/controllers/npController.js
+++ server/controllers/npController.js
@@ -2,8 +2,8 @@ const db = require('../../models/npModels');
 
 const npController = {};
 
-npController.getParks = (req, res, next) => {
-  const bucketlist = 'Select parkname FROM bucketlist;';
+npController.getParksFromBucketList = (req, res, next) => {
+  const bucketlist = 'Select * FROM bucketlist;';
   db.query(bucketlist)
     .then((data) => {
       console.log(data);
@@ -16,4 +16,29 @@ npController.getParks = (req, res, next) => {
       next();
     });
 };
+
+npController.bucketlistAdd = (req, res, next) => {
+  // const { fullName, parkCode, latitude, longitude } = req.body.bucketListParks;
+  req.body.bucketListPark.forEach((park) => {
+    const bucketlistAdd = `INSERT INTO bucketlist(parkname, parkcode, lat, long)
+  VALUES($1, $2, $3, $4))`;
+    const values = [
+      park.fullName,
+      park.parkCode,
+      park.latitude,
+      park.longitude,
+    ];
+
+    db.query(bucketlistAdd, values)
+      .then((data) => {
+        console.log(`from add ${data}`);
+        next();
+      })
+      .catch((err) => {
+        console.log(`error in bucketlistAdd ${park.fullName}:`, err);
+        next({ log: err });
+      });
+  });
+};
+
 module.exports = npController;
--- server/routes/dbRouter.js
+++ server/routes/dbRouter.js
@@ -4,8 +4,17 @@ const natlParks = require('../../models/npModels');
 const npController = require('../controllers/npController');
 const dbRouter = express.Router();
 
-dbRouter.get('/', npController.getParks, (req, res) =>
-  res.status(200).json(res.locals.parks)
+dbRouter.get(
+  '/initial-render',
+  npController.getParksFromBucketList,
+  (req, res) => res.status(200).json(res.locals.parks)
+);
+
+dbRouter.get(
+  '/put-bucketlist',
+  npController.bucketlistAdd,
+  npController.getParksFromBucketList,
+  (req, res) => res.status(200).json(res.locals.bucketlist)
 );
 
 module.exports = dbRouter;
