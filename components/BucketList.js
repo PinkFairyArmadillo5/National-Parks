d56a2414a6424a7a4b7747517f254f8b444599ed
Julia Hickey <hijulia1136@Julias-MacBook-Pro.local>
2022-01-04 19:27:43 -0800

    updated with red x trash button in bucketlistjs

--- components/BucketList.js
+++ components/BucketList.js
@@ -12,18 +12,45 @@ function BucketList({ bucketList, setBucketList }) {
         setBucketList(updatedBL);
         console.log('BucketList.js line 13', updatedBL);
       });
-  }, []
-  );
+  }, []);
+
+  //const [ updatedBucketList, setUpdatedBucketList ] = useState(bucketList)
+
+  const handleDelete = (e) => {
+    const parkToDelete = e.target.getPark(bucketList.parkcode);
+    setUpdatedBucketList(
+      bucketList.filter((park) => park.parkcode !== parkToDelete)
+    );
+  };
 
   return (
     <div>
       <h3>My Bucket List</h3>
       {bucketList.map((park, index) => (
-        <div key={index}>{park.parkname}</div>
-      )
-      )}
+        <div key={index}>
+          <span
+            className='deleteX'
+            parkcode={park.parkcode}
+            onClick={handleDelete}
+          >
+            X
+          </span>
+          {park.parkname}
+        </div>
+      ))}
     </div>
   );
 }
 
 export default BucketList;
+
+/*delete route tasks
+find trash or X icon to render with each park listed
+onclick to remove park from bucketlist
+  sends data (post request) to remove park from bucketlist SQL table
+  returns edited table
+router - dbRouter
+SQL query to drop row where parkID matches or parkCode
+CSS style to format bucketlist rendering with room for icon
+
+*/
